b697b68bed18ac19d9001a8fa1a9da2e854e47a6
pauloeduardoprimosilva-hue <pauloeduardoprimosilva@gmail.com>
2026-07-30 20:11:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mysteryBox').addEventListener('click', function() {
+    // Adiciona a classe 'revealed' no container principal da surpresa
+    this.classList.add('revealed');
+});
