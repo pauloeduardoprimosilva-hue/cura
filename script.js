042d9c3c1d4e8c99f1ca3d5ba5760f570e55ddf4
pauloeduardoprimosilva-hue <pauloeduardoprimosilva@gmail.com>
2026-07-30 20:10:14 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('mysteryBox').addEventListener('click', function() {
-    // Adiciona a classe 'revealed' no container principal da surpresa
-    this.classList.add('revealed');
-});
